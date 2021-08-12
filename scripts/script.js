054060e6c4da6e567db64ac518d90702519a31a1
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-12 19:27:43 +0400

    feat: сохранил данные в localStorage и сделал автоматическую подгрузку данных

--- scripts/script.js
+++ scripts/script.js
@@ -2,59 +2,74 @@
 
 const todoControl = document.querySelector('.todo-control'),
   headerInput = document.querySelector('.header-input'),
-  todoList = document.querySelector('.todo-list'),
-  todoCompleted = document.querySelector('.todo-completed');
+  todoList = document.querySelector('#todo'),
+  todoCompleted = document.querySelector('#completed');
 
-const todoData = [];
+let todoData = [];
+
+const getLocal = () => {
+  localStorage.setItem('todoLists', JSON.stringify(todoData));
+};
 
 const render = () => {
   todoList.textContent = '';
   todoCompleted.textContent = '';
 
-  todoData.forEach((item) => {
+  todoData = JSON.parse(localStorage.getItem('todoLists')) || [];
+
+  todoData.forEach((item, index) => {
+
     let li = document.createElement('li');
     li.classList.add('todo-item');
 
     li.innerHTML = `
-    <span class="text-todo">${item.value}</span>
-    <div class="todo-buttons">
-        <button class="todo-remove"></button>
-        <button class="todo-complete"></button>
-    </div>
-    `;
-    
+        <span class="text-todo">${item.value}</span>
+        <div class="todo-buttons">
+            <button class="todo-remove"></button>
+            <button class="todo-complete"></button>
+        </div>
+        `;
+
     if (item.completed) {
       todoCompleted.append(li);
     } else {
       todoList.append(li);
     }
 
-    const btnTodoComplete = li.querySelector('.todo-complete');
-
+    let btnTodoComplete = li.querySelector('.todo-complete');
     btnTodoComplete.addEventListener('click', () => {
       item.completed = !item.completed;
+
+      getLocal();
       render();
-    })
+    });
+
+    let btnTodoRemove = li.querySelector('.todo-remove');
+    btnTodoRemove.addEventListener('click', () => {
+      todoData.splice(index, 1);
+
+      getLocal();
+      render();
+    });
+
   });
+
 };
 
 todoControl.addEventListener('submit', (event) => {
   event.preventDefault();
+  if (headerInput.value !== '') {
+    const newTodo = {
+      value: headerInput.value,
+      completed: false,
+    };
 
-  const newTodo = {
-    value: headerInput.value,
-    completed: false
-  };
-  
-  if (!newTodo.value) {
-    return;
+    headerInput.value = '';
+
+    todoData.push(newTodo);
+    getLocal();
+    render();
   }
-  
-  todoData.push(newTodo);
-  
-  headerInput.value = '';
-  
-  render();
 });
 
 render();
